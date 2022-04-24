ec92755c82fc3453038e6f3359c9c1658a3420bc
Mohit Raj <mohitraj3125@gmail.com>
2022-04-24 14:58:16 +0530

    commit

--- Code/pattern13hollow_inverted_halfpyramid.cpp
+++ Code/pattern13hollow_inverted_halfpyramid.cpp
@@ -7,10 +7,16 @@ int main()
         int i,j;
       for(i=1;i<=n;i++)
       {
-          for(j=1;j<=i;j++)
+          for(j=i;j<=n;j++)
           {
-              if( j==1 || i==n || j==i)
+              if(i==1)
               cout<<j<<" ";
+
+              else if(j==i)
+              cout<<i<<" ";
+
+              else if(j==n)
+              cout<<n<<" ";
 
               else
               cout<<"  ";
