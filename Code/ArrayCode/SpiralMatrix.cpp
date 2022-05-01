5dba0699dbb1676d2e6f9d57d38e90f191d1e6a4
Mohit Raj <mohitraj3125@gmail.com>
2022-05-01 13:21:12 +0530

    commit

---
+++ Code/ArrayCode/SpiralMatrix.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int count=0;
+    int total=n*n;
+    while(count<total)
+    {
+        //Starting row of matrix
+        for(int i=0startingCol; )
+    }
+}
