2f3fc1494832b15c071ad298492d784fba2531fb
Mohit Raj <mohitraj3125@gmail.com>
2022-05-01 22:19:39 +0530

    commit

--- Code/ArrayCode/SpiralMatrix.cpp
+++ Code/ArrayCode/SpiralMatrix.cpp
@@ -3,11 +3,48 @@ using namespace std;
 
 int main()
 {
-    int count=0;
-    int total=n*n;
+    int row=3 , col=3;
+    int arr[3][3]= {1,2,3,4,5,6,7,8,9};
+    int count = 0;
+    int total = row*col;
+    int sr = 0; //sr=startingRow
+    int sc = 0; //sc=startingCol
+    int ec = col-1; //ec=endingCol
+    int er = row-1; //er=endingRow
+
     while(count<total)
     {
-        //Starting row of matrix
-        for(int i=0startingCol; )
+        //First part(starting row)
+        for(int i=sc; i<=ec; i++)
+        {
+            cout<<arr[sr][i]<<" ";
+            count++;
+        }
+        sr++;
+
+        //second part(ending col)
+        for(int i=sr; i<=er; i++)
+        {
+            cout<<arr[i][ec]<<" ";
+            count++;
+        }
+        ec--;
+
+        //Third part(ending row)
+        for(int i=ec; i>=sc; i--)
+        {
+            cout<<arr[er][i]<<" ";
+            count++;
+        }
+        er--;
+
+        //Fourth part(starting col)
+        for(int i=er; i>=sr; i--)
+        {
+            cout<<arr[i][sc]<<" ";
+            count++;
+        }
+        sc++;
     }
+    return 0;
 }
