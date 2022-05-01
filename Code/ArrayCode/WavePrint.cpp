60051cd10c4ffaf09bc472b20247a417fdd28e9e
Mohit Raj <mohitraj3125@gmail.com>
2022-05-01 22:38:38 +0530

    commit

--- Code/ArrayCode/WavePrint.cpp
+++ Code/ArrayCode/WavePrint.cpp
@@ -4,16 +4,16 @@ using namespace std;
 void WavePrint(int m, int n, int arr[3][3])
 {
     // Loop to traverse matrix
-    for (int i = 0; i < n; i++) {
+    for (int j = 0; j < n; j++) {
         // it in forward order
-        if (i % 2 == 0) {
-            for (int j = 0; j < m; j++) {
+        if (j % 2 == 0) {
+            for (int i = 0; i < m; i++) {
                 cout << arr[i][j] << " ";
             }
         }
         // it in reverse order
         else {
-            for (int j = m - 1; j >= 0; j--) {
+            for (int i = m - 1; i >= 0; i--) {
                 cout << arr[i][j] << " ";
             }
         }
@@ -22,6 +22,8 @@ void WavePrint(int m, int n, int arr[3][3])
 int main()
 {
     int arr[3][3] = {1,2,3,4,5,6,7,8,9};
+ 
     WavePrint(3, 3, arr);
+ 
     return 0;
 }
