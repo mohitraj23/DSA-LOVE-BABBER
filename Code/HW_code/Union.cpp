dc4dddb9607a38297180d1b9bf0d5a1338ad058f
Mohit Raj <mohitraj3125@gmail.com>
2022-05-03 14:24:19 +0530

    commit

---
+++ Code/HW_code/Union.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// Function to find intersection
+void intersection(int a[], int b[], int n, int m)
+{
+	int i = 0, j = 0;
+	while (i < n && j < m) {
+		if (a[i] > b[j]) {
+			j++;
+		}
+		else if (b[j] > a[i]) {
+			i++;
+		}
+		else {
+			
+			// when both are equal
+			cout << a[i] << " ";
+			i++;
+			j++;
+		}
+	}
+}
+
+// Driver Code
+int main()
+{
+	int a[] = { 1, 3, 2, 3, 3, 4, 5, 5, 6 };
+	int b[] = { 3, 3, 5 };
+
+	int n = sizeof(a) / sizeof(a[0]);
+	int m = sizeof(b) / sizeof(b[0]);
+
+	// sort
+	sort(a, a + n);
+	sort(b, b + m);
+
+	// Function call
+	intersection(a, b, n, m);
+}
