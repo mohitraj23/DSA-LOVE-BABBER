a30b3565acadb33cfa3290cbb86cc0a96c4c794d
Mohit Raj <mohitraj3125@gmail.com>
2022-05-03 14:50:13 +0530

    commit

--- Code/HW_code/Union.cpp
+++ Code/HW_code/Union.cpp
@@ -1,41 +1,49 @@
 #include <bits/stdc++.h>
-
 using namespace std;
 
-// Function to find intersection
-void intersection(int a[], int b[], int n, int m)
+/* Function prints union of arr1[] and arr2[]
+m is the number of elements in arr1[]
+n is the number of elements in arr2[] */
+void printUnion(int arr1[], int arr2[], int m, int n)
 {
 	int i = 0, j = 0;
-	while (i < n && j < m) {
-		if (a[i] > b[j]) {
-			j++;
-		}
-		else if (b[j] > a[i]) {
-			i++;
-		}
+	while (i < m && j < n) {
+		if (arr1[i] < arr2[j])
+			cout << arr1[i++] << " ";
+
+		else if (arr2[j] < arr1[i])
+			cout << arr2[j++] << " ";
+
 		else {
-			
-			// when both are equal
-			cout << a[i] << " ";
+			cout << arr2[j++] << " ";
 			i++;
-			j++;
 		}
 	}
+
+	/* Print remaining elements of the larger array */
+	while (i < m)
+		cout << arr1[i++] << " ";
+
+	while (j < n)
+		cout << arr2[j++] << " ";
 }
 
-// Driver Code
+/* Driver program to test above function */
 int main()
 {
-	int a[] = { 1, 3, 2, 3, 3, 4, 5, 5, 6 };
-	int b[] = { 3, 3, 5 };
+	int arr1[] = { 1, 5,4, 4, 5, 6 };
+	int arr2[] = { 2, 3, 5, 7 };
 
-	int n = sizeof(a) / sizeof(a[0]);
-	int m = sizeof(b) / sizeof(b[0]);
+	int m = sizeof(arr1) / sizeof(arr1[0]);
+	int n = sizeof(arr2) / sizeof(arr2[0]);
 
-	// sort
-	sort(a, a + n);
-	sort(b, b + m);
 
-	// Function call
-	intersection(a, b, n, m);
+    //sort
+    sort(arr1, arr1 + m);
+	sort(arr2, arr2 + n);
+
+	// Function calling
+	printUnion(arr1, arr2, m, n);
+
+	return 0;
 }
