549f27338e8c9c7f92d1f5265d0ebacf03dc77a6
Mohit Raj <mohitraj3125@gmail.com>
2022-05-21 01:07:40 +0530

    commit

--- Code/Char_Array&Strings/HW_sheet_code/2_Reverse_string.cpp
+++ Code/Char_Array&Strings/HW_sheet_code/2_Reverse_string.cpp
@@ -7,9 +7,26 @@ string ReverseWord(string str)
     return str;
 }
 
+void Reverse(char arr[])
+{
+    int n=strlen(arr);
+    int s=0, e=n-1;
+
+    while(s<=e)
+    {
+        swap(arr[s], arr[e]);
+        s++;
+        e--;
+    }
+}
+
 int main()
 {
     string str = "Mohit Raj";
-    cout<<ReverseWord(str);
+    cout<<ReverseWord(str)<<endl;
+
+    char arr[]={"Mohit Raj"};
+    Reverse(arr);
+    cout<<arr<<endl;
     return 0;
 }
