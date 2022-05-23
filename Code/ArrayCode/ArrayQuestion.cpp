70354dbd8cd186f048cb031b49259dd86e0b1220
Mohit Raj <mohitraj3125@gmail.com>
2022-05-24 01:12:34 +0530

    commit

--- Code/ArrayCode/ArrayQuestion.cpp
+++ Code/ArrayCode/ArrayQuestion.cpp
@@ -1,4 +1,4 @@
-#include<iostream>
+#include<bits/stdc++.h>
 using namespace std;
 
 void reverseArray(int arr[], int n)
---
+++ Code/Char_Array&Strings/HW_sheet_code/3_Check_palindrome.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+bool isPalindrome(int n)
+{
+    
+}
+
+int main()
+{
+
+    return 0;
+}
